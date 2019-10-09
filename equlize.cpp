ea2920e08ee1f6589419981c154b6a73e2dd990a
kittu2539 <56120062+kittu2539@users.noreply.github.com>
2019-10-09 18:25:08 +0530

    Add files via upload

---
+++ equlize.cpp
@@ -0,0 +1,33 @@
+#include <cmath> 
+#include <iomanip> 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int q;
+	cin>>q;
+	int a[q];
+	int c[q];
+	for(int i=0;i<q;i++)
+	{
+		cin>>a[i];
+		c[i]=0;
+		int b[a[i]];
+		for(int j=0;j<a[i];j++)
+		{
+			cin>>b[j];
+			c[i]=c[i]+b[j];
+		}
+		if(c[i]%a[i]==0)
+		{
+			cout<<c[i]/a[i]<<endl;
+		}
+		else
+		{
+			cout<<c[i]/a[i]+1<<endl;
+		}
+	}
+	
+}
